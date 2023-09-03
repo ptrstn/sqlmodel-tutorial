ad4e9d9132c5f3fa70b11026f6b2be4ef325b7a6
Peter Stein <peterstein@protonmail.com>
2023-09-03 21:06:55 +0200

    Create database data

--- tutorial/app.py
+++ tutorial/app.py
@@ -1,5 +1,6 @@
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Session
 
+from .models import Hero
 from . import models  # noqa: F401
 from .db import engine
 
@@ -8,5 +9,24 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-if __name__ == "__main__":
+def create_heroes():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+    session = Session(engine)
+
+    session.add(hero_1)
+    session.add(hero_2)
+    session.add(hero_3)
+
+    session.commit()
+
+
+def main():
     create_db_and_tables()
+    create_heroes()
+
+
+if __name__ == "__main__":
+    main()
