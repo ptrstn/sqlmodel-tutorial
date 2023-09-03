d9052986baf64ac5af137f529fbf6b0e1484904d
Peter Stein <peterstein@protonmail.com>
2023-09-03 20:13:11 +0200

    Create Python package "tutorial"

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="sqlmodel-tutorial",
+    version="0.0.1",
+    url="https://github.com/ptrstn/sqlmodel-tutorial",
+    author="Peter Stein",
+    license="MIT",
+    python_requires='>3.7',
+    packages=["tutorial"],
+    install_requires=["sqlmodel"],
+)
